d6a17ee8e8e95f2e3bb64d6b1a9fa700866cc134
Clayton Glasgow <cglasgow@nd.edu>
2022-12-02 10:53:41 -0500

    final project scripts

---
+++ analysis.R
@@ -0,0 +1,4 @@
+## Intro to Biocomputing R Final Project: Analysis
+## Clayton Glasgow and Sidney Harris
+## 14 December 2022
+
---
+++ supportingFunctions.R
@@ -0,0 +1,4 @@
+## Intro to Biocomputing Final R Project: Supporting Functions
+## Clayton Glasgow and Sidney Harris
+## 14 December 2022
+
