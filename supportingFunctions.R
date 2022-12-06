6c253c0af298061e65dbfda2b1e382de01fd0786
midwesternmouse <gretandreasen@gmail.com>
2022-12-06 11:04:46 -0500

    Added comments and a thought about using an if statement -Gret

--- supportingFunctions.R
+++ supportingFunctions.R
@@ -12,3 +12,24 @@ for (i in 1:length(filelist)){
   write.table(data, file=output, sep="," , col.names = TRUE, row.names=FALSE)
   setwd("~/Desktop/Biocomputing_RProject-main/Rproject2022/countryY/")
 }
+
+# Gretchen notes:
+# I think we should use if statements to make our function applicable to all
+# file types.
+# Something like...
+library(tools) # This loads a library with the function below
+?file_ext # This function can pull a file type just from a path
+# Now I set it to my working directory (just to test it)
+setwd("~/Documents/Courses/Biocomputing_BIOS_60318/Tutorials/Biocomputing_RProject/Rproject2022/countryX")
+# And use the function to get the file type of screen_120.csv!
+file_ext("~/Documents/Courses/Biocomputing_BIOS_60318/Tutorials/Biocomputing_RProject/Rproject2022/countryX/screen120.csv")
+# The function returns "csv"
+
+# So maybe we could do something like..
+for (file in filelist){
+	if (file_ext(file) = "csv"){
+		# blah blah blah
+	}else if (file_ext(file) = "txt"){
+		# blah blah blah
+	}
+}
