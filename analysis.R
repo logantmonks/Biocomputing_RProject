032ccff49e14ce7f5bf9ac7b8963ddc347a9d782
Clayton Glasgow <cglasgow@nd.edu>
2022-12-08 23:03:04 -0500

    analysis

--- analysis.R
+++ analysis.R
@@ -1,4 +1,11 @@
 ## Intro to Biocomputing R Final Project: Analysis
-## Clayton Glasgow and Sidney Harris
+## Clayton Glasgow, Sydney Harris, Jaden Bailey
 ## 14 December 2022
 
+## set working directory
+# maybe let's type another brief paragraph describing our file structure like last time?
+setwd("~/Desktop/IntroBiocomputing/R/Biocomputing_RProject/Rproject2022")
+
+
+## load functions from supportingFunctions script
+#source(supportingFunctions.R)
