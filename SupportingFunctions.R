d9086d599acb05bde9c56f8495f1923224b952fc
Troy McFarland <tmcfarl2@nd.edu>
2022-12-14 21:42:29 -0500

    added third function, incomplete

--- SupportingFunctions.R
+++ SupportingFunctions.R
@@ -41,7 +41,7 @@ txt2csv <- function(wd = NULL, deltxt = NULL) {
 #   .csv files:
 #       allData.csv <- rbind([filepath]/Country_Y.csv, [filepath]/Country_X.csv)
 
-# argument countryname should describe what country the data originates from.
+# argument countryname should describe what country the data originates from
 #   and is best used with no spaces.
 
 # argument omit.NA will remove rows with NA values from combined .csv result
@@ -97,5 +97,98 @@ else if (is.null(omit.NA)) {
 }
 
 # FUNCTION 3
+overview <- function(filename) {
+  all_data <- read.csv(filename)
+  total_screens <- nrow(all_data)
+  N_females_screened <- nrow(all_data[all_data$gender == "female",])
+  N_males_screened <- nrow(all_data[all_data$gender == "male",])
+  
+  infected_data <- data.frame(matrix(ncol = 0, nrow = 0))
+  healthy_data <- data.frame(matrix(ncol = 0, nrow = 0))
+  total_infected <- 0
+  N_marker01 <- 0
+  N_marker02 <- 0
+  N_marker03 <- 0
+  N_marker04 <- 0
+  N_marker05 <- 0
+  N_marker06 <- 0
+  N_marker07 <- 0
+  N_marker08 <- 0
+  N_marker09 <- 0
+  N_marker10 <- 0
+  
+  for (i in 1:nrow(all_data)) {
+    
+    if (all_data$marker01[i] > 0 || all_data$marker02[i] > 0 || all_data$marker03[i] > 0 || all_data$marker04[i] > 0 || all_data$marker05[i] > 0 || all_data$marker06[i] > 0 || all_data$marker07[i] > 0 || all_data$marker08[i] > 0|| all_data$marker09[i] > 0|| all_data$marker10[i] > 0) {
+      infected_data <- rbind(infected_data, all_data[i,])
+    }
+    else {
+      healthy_data <- rbind(healthy_data, all_data[i,])
+    }
+    
+    if (all_data$marker01[i] > 0){
+      N_marker01 <- N_marker01 + 1
+    }
+    if (all_data$marker02[i] > 0){
+      N_marker02 <- N_marker02 + 1
+    }
+    if (all_data$marker03[i] > 0){
+      N_marker03 <- N_marker03 + 1
+    }
+    if (all_data$marker04[i] > 0){
+      N_marker04 <- N_marker04 + 1
+    }
+    if (all_data$marker05[i] > 0){
+      N_marker05 <- N_marker05 + 1
+    }
+    if (all_data$marker06[i] > 0){
+      N_marker06 <- N_marker06 + 1
+    }
+    if (all_data$marker07[i] > 0){
+      N_marker07 <- N_marker07 + 1
+    }
+    if (all_data$marker08[i] > 0){
+      N_marker08 <- N_marker08 + 1
+    }
+    if (all_data$marker09[i] > 0){
+      N_marker09 <- N_marker09 + 1
+    }
+    if (all_data$marker10[i] > 0){
+      N_marker10 <- N_marker10 + 1
+    }
+    
+  }
+  
+  total_infected <- nrow(infected_data)
+  percent_infected <- total_infected / total_screens * 100
+  N_females_infected <- nrow(infected_data[infected_data$gender == "female",])
+  N_males_infected <- nrow(infected_data[infected_data$gender == "male",])
+  percent_females_infected <- N_females_infected / N_females_screened * 100
+  percent_males_infected <- N_males_infected / N_males_screened * 100
+  
+  print(paste0("Total infected: ", total_infected))
+  print(paste0("Total screened:", total_screens))
+  print(paste0("Total infected: ", total_infected))
+  
+  
+  # Age distribution histogram
+  # extreme outliers in age, such as some individuals living to +400 y.o.a, exist
+  #   and make plotting difficult. Thus, ages up to 123 are plotted, though all data
+  #   will be written to output files.
+  
+  healthy_data_trim <- healthy_data[healthy_data$age < 123,]
+  infected_data_trim <- infected_data[infected_data$age < 123,]
+  
+  
+  
+  library(ggplot2)
+ ggplot() +
+    geom_histogram(data=infected_data_trim, binwidth = 1, aes(x=age), alpha = 0.6, fill = "red") +
+    geom_histogram(data=healthy_data_trim, binwidth = 1, aes(x=age), alpha = 0.6, fill = "green") +
+    scale_color_manual(name = "Status", breaks = c("infected", "healthy"), values = c("infected" = "red", "healthy" = "green")) +
+    ggtitle("Infected (red) and healthy (green) patient age distribution")
+}
+
+
 
 
